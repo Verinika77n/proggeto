97f13c65fe981d8db46c5f426c92c5136f2793af
Veronica <verinika77n@gmail.com>
2025-12-07 16:01:00 +0500

    Рефакторинг структуры кода для повышения читабельности и удобства сопровождения

--- profiles/views.py
+++ profiles/views.py
@@ -11,6 +11,8 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import logout  
+
 
 class SearchMixin:
     def get_queryset(self):
@@ -28,20 +30,12 @@ class EntryListView(SearchMixin, ListView):
     model = DataUser
     template_name = 'profiles/entry_list.html'
     #context_object_name = 'page_obj'
-    paginate_by = 5
+    paginate_by = 6
 
 class EntryDetailView(DetailView):
     model = DataUser
     template_name = 'profiles/entry_detail.html'
     context_object_name = 'entry'
-
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect
-from django.urls import reverse
-from django.contrib import messages
-from django.views.generic import CreateView
-from .models import DataUser
-from .forms import DataUserForm
 
 class EntryCreateView(LoginRequiredMixin, CreateView):
     model = DataUser
@@ -61,7 +55,7 @@ class EntryCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
     def get_success_url(self):
-        messages.success(self.request, 'Запись добавлена')
+        messages.success(self.request, 'Данные обновлены')
         return reverse('profiles-detail', args=[self.object.pk])
 
 class OwnerOrStaffRequired(UserPassesTestMixin):
@@ -82,17 +76,26 @@ class EntryUpdateView(LoginRequiredMixin, UpdateView):
         return self.request.user.profile
 
     def get_success_url(self):
-        messages.success(self.request, 'Запись обновлена')
+        messages.success(self.request, 'Данные обновлены')
         return reverse('profiles-detail', args=[self.object.pk])
 
 class EntryDeleteView(LoginRequiredMixin, OwnerOrStaffRequired, DeleteView):
     model = DataUser
     template_name = 'profiles/entry_confirm_delete.html'
     success_url = reverse_lazy('profiles')
-    def delete(self, request, *args, **kwargs):
-        from django.contrib import messages
-        messages.success(self.request, 'Запись удалена')
-        return super().delete(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        user = self.object.user
+        if user.is_staff or user.is_superuser:
+            self.object.delete()
+            messages.success(request, 'Профиль удалён')
+            print(123)
+            return redirect('profiles')
+        logout(request)              
+        user.delete()       
+        print(456)        
+        messages.success(request, 'Профиль и аккаунт удалены')
+        return redirect('login')     
 
 class MyProfileView(LoginRequiredMixin, DetailView):
     model = DataUser
@@ -100,11 +103,9 @@ class MyProfileView(LoginRequiredMixin, DetailView):
     context_object_name = 'entry'
 
     def get_object(self, queryset=None):
-        # если профиль есть — возвращаем его
         try:
             return self.request.user.profile
         except DataUser.DoesNotExist:
-            # если профиля нет — редиректим на создание
             return redirect('profiles-create')
 
 #
